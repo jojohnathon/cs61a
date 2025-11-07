b5805570806285dd6e70ca28b39a3b5a86ae815a
jojohnathon <pixelizedjohnny@gmail.com>
2025-11-06 18:53:45 -0800

    add lab 8

---
+++ labs/lab08/lab08.scm
@@ -0,0 +1,34 @@
+(define (over-or-under num1 num2) 
+  (if (< num1 num2) -1
+  (if (> num1 num2) 1 0))
+)
+
+(define (make-adder num) 
+  (define (adder k) (+ k num))
+  adder
+)
+
+(define (composed f g)
+  (lambda (x) (f (g x)))
+)
+
+(define (repeat f n) 
+    (if (= n 0) 
+      (lambda (x) x) 
+      (composed f (repeat f (- n 1))))
+)
+
+(define (max a b)
+  (if (> a b)
+      a
+      b))
+
+(define (min a b)
+  (if (> a b)
+      b
+      a))
+
+(define (gcd a b) 
+      (if (zero? (modulo (max a b) (min a b)))
+      (min a b)
+      (gcd (min a b) (modulo (max a b) (min a b)))))
