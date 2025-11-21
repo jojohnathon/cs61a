7200732947baa24cd613c8be2ed517fe8b17610e
jojohnathon <pixelizedjohnny@gmail.com>
2025-11-20 22:17:11 -0800

    lab 10

--- projects/scheme/scheme_eval_apply.py
+++ projects/scheme/scheme_eval_apply.py
@@ -36,6 +36,9 @@ def scheme_eval(expr, env, _=None): # Optional third argument is ignored
     else:
         # BEGIN PROBLEM 3
         "*** YOUR CODE HERE ***"
+        procedure = scheme_eval(first, env)
+        args = map_link(lambda operand: scheme_eval(operand, env), rest)
+        return scheme_apply(procedure, args, env)
         # END PROBLEM 3
 
 def scheme_apply(procedure, args, env):
@@ -47,10 +50,19 @@ def scheme_apply(procedure, args, env):
     if isinstance(procedure, BuiltinProcedure):
         # BEGIN PROBLEM 2
         "*** YOUR CODE HERE ***"
+        pargs = []
+        while args is not Link.empty:
+            pargs.append(args.first)
+            print("DEBUG: ", pargs)
+            args = args.rest
+        if procedure.need_env:
+            pargs.append(env)
         # END PROBLEM 2
         try:
             # BEGIN PROBLEM 2
             "*** YOUR CODE HERE ***"
+            print("DEBUG: ", pargs)
+            return procedure.py_func(*pargs)
             # END PROBLEM 2
         except TypeError as err:
             raise SchemeError('incorrect number of arguments: {0}'.format(procedure))
