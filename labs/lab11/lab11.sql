ba6cc46c89b0317f715b66e32c6b45045fa2e51a
jojohnathon <pixelizedjohnny@gmail.com>
2025-12-03 15:45:38 -0800

    do lab 11

--- labs/lab11/lab11.sql
+++ labs/lab11/lab11.sql
@@ -1,15 +1,28 @@
 CREATE table newest AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT title, year 
+  FROM titles
+  ORDER BY year DESC
+  LIMIT 10;
 
 
 CREATE table dog_movies AS 
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT title, character
+  FROM titles JOIN principals 
+  ON titles.tconst = principals.tconst
+  WHERE character LIKE "%dog%";
 
 
 CREATE table leads AS 
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT name, count(*) as lead_roles
+  FROM names JOIN principals ON names.nconst = principals.nconst
+  WHERE ordering = 1
+  GROUP BY names.nconst 
+  HAVING count(*) > 10;
 
 
 CREATE table long_movies AS 
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT ((year / 10) * 10) || "s" as decade, count(*) as count
+  FROM titles
+  WHERE runtime > 180
+  GROUP BY decade;
 
